882e93b9409fa16ea26787b47c78bd8770e5f419
mind1949 <lianjie1949@gmail.com>
2020-04-24 09:53:19 +0800

    实现快速排序的随机化版本

--- algorithms/algorithms.go
+++ algorithms/algorithms.go
@@ -1,5 +1,10 @@
 package algorithms
 
+import (
+	"math/rand"
+	"time"
+)
+
 func IsSorted(slice []int) bool {
 	n := len(slice)
 	for i := 0; i < n-1; i++ {
@@ -8,4 +13,9 @@ func IsSorted(slice []int) bool {
 		}
 	}
 	return true
+}
+
+func Random(p, r int) int {
+	rand.Seed(time.Now().Unix())
+	return rand.Intn(r-p+1) + p
 }
--- algorithms/quick_sort.go
+++ algorithms/quick_sort.go
@@ -5,13 +5,24 @@ func QuickSort(s []int, p, r int) {
 	if p >= r {
 		return
 	}
-	q := partition(s, p, r)
+	q := randomizedPartition(s, p, r)
 	QuickSort(s, p, q-1)
 	QuickSort(s, q+1, r)
 }
 
+// randomizedPartition 随机分区
+// 通过随机抽样似的对于任何输入都能获得较好的期望性能
+func randomizedPartition(s []int, p, r int) (q int) {
+	i := Random(p, r)
+	s[i], s[r] = s[r], s[i]
+	return partition(s, p, r)
+}
+
 // partition 分区
-func partition(s []int, p, r int) int {
+// 经过分区处理后
+// s[p:q]中任一元素小于或等于s[q]
+// s[q+1:r]中任一元素大于s[q]
+func partition(s []int, p, r int) (q int) {
 	x := s[r]
 	i := p - 1
 	for j := p; j < r; j++ {
@@ -23,3 +34,9 @@ func partition(s []int, p, r int) int {
 	s[i+1], s[r] = s[r], s[i+1]
 	return i + 1
 }
+
+/*
+证明partition函数的正确性
+* 循环不变式:
+s[p:i]中任一元素≤主元x, s[i:j]中任一元素≥主元,
+*/
